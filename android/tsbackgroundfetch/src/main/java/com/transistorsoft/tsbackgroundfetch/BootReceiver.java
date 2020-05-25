d67d892ee077bb27ed3d153414584ec547cfc6b9
Chris Scott <christocracy@gmail.com>
2020-05-25 10:09:02 -0400

    onBoot must take into account forceAlarmManager and auto-start those tasks

--- android/tsbackgroundfetch/src/main/java/com/transistorsoft/tsbackgroundfetch/BootReceiver.java
+++ android/tsbackgroundfetch/src/main/java/com/transistorsoft/tsbackgroundfetch/BootReceiver.java
@@ -3,7 +3,6 @@ package com.transistorsoft.tsbackgroundfetch;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 
 /**
@@ -14,15 +13,12 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        // Android SDK >= LOLLIPOP_MR1 use JobScheduler which automatically persists Jobs on BOOT
         String action = intent.getAction();
-        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1) {
-            Log.d(BackgroundFetch.TAG,  "BootReceiver: " + action);
-            BackgroundFetch.getThreadPool().execute(new Runnable() {
-                @Override public void run() {
-                    BackgroundFetch.getInstance(context.getApplicationContext()).onBoot();
-                }
-            });
-        }
+        Log.d(BackgroundFetch.TAG,  "BootReceiver: " + action);
+        BackgroundFetch.getThreadPool().execute(new Runnable() {
+            @Override public void run() {
+                BackgroundFetch.getInstance(context.getApplicationContext()).onBoot();
+            }
+        });
     }
 }
